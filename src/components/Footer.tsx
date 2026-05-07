905439871f47d3a2d12cc7c85f346331ff67fec8
Casey Mogilevsky <141592509+caseysm@users.noreply.github.com>
2026-05-07 15:32:30 -0400

    footer: yuji syuku + lowercase

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,9 +9,9 @@
 export default function Footer() {
   return (
     <>
-      <div className="fixed left-5 landscape:left-10 bottom-9 z-[2] font-ibm font-light uppercase text-[9px] landscape:text-[11px] tracking-footer text-wisteria-600" />
+      <div className="fixed left-5 landscape:left-10 bottom-9 z-[2] font-yuji font-light text-[9px] landscape:text-[11px] tracking-footer text-wisteria-600" />
 
-      <div className="fixed right-5 landscape:right-10 bottom-9 z-[2] font-ibm font-light uppercase text-[9px] landscape:text-[11px] tracking-footer text-wisteria-600">
+      <div className="fixed right-5 landscape:right-10 bottom-9 z-[2] font-yuji font-light text-[9px] landscape:text-[11px] tracking-footer text-wisteria-600">
         <a
           href="mailto:info@orikata.ai"
           className="text-wisteria-600 no-underline transition-colors duration-500 ease-[cubic-bezier(0.4,0,0.2,1)] hover:text-wisteria-700"
